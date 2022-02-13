7f203a1d199b30a7c3c96e49f09a6288b66508fc
Krishnagupta <55663318+Krishnagupta27@users.noreply.github.com>
2022-02-13 12:46:01 +0530

    Add files via upload

---
+++ Assignment 1/app.js
@@ -0,0 +1,35 @@
+(function() {
+  "use strict";
+
+  angular.module("LunchCheck", []).controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController($scope) {
+    $scope.itemString = "";
+    $scope.nbOfItems = 0;
+    $scope.resultString;
+    $scope.resultOk = null;
+    $scope.checkItems = function() {
+      if($scope.itemString.length > 0) {
+        console.log("Items entered: " + $scope.itemString);
+        var itemArray = $scope.itemString.split(",");
+        console.log("Items as array: " + itemArray);
+        $scope.resultString = checkItemCount(itemArray);
+        $scope.resultOk = true;
+      }
+      else
+      {
+        $scope.resultString = "Please enter data first";
+        $scope.resultOk = false;
+      }
+    };
+  }
+  function checkItemCount(items) {
+    var itemCount = items.length;
+    console.log("Number of items entered: " + itemCount);
+    if(itemCount < 4)
+      return "Enjoy!";
+    else
+      return "Too much!";
+  }
+})();
